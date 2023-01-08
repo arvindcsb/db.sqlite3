931334934ac812435174216952e513c577b98b66
arvindcsb <arvindcsb46@gmail.com>
2023-01-08 22:56:02 +0530

    ice

--- adminsapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from customerapp.models import Customer,Booking,Rating
-from salonapp.models import Salons
-
-# Create your models here.
-class Admins(models.Model):
-    fullname=models.CharField(max_length=100)
-    password=models.CharField(max_length=100)
